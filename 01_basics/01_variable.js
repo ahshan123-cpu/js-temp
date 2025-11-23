60940660b13fba81ce62cd2fd3cc30835ad0f2bf
alam <you@example.com>
2025-11-23 19:08:26 +0530

    dataTypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,6 @@
+const accoundId = 123321
+let accountEmail = "ahshan@google.com"
+var accountPassword = "manage"
+accountCity = "Hyderabad"
+
+console.table([accoundId, accountEmail, accountPassword, accountCity])
