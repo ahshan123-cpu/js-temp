7405d6fb7fa21853cd884286b1cba5a76c60853e
alam <you@example.com>
2025-11-23 19:44:31 +0530

    updated

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,14 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+console.log(2**3);
+console.log(2*3);
+console.log(2/6);
+
+let str1 = "hello"
+let str2 = "Ahshan"
+
+let str3 = str1+" "+str2
+console.log(str3);
+
+
---
+++ 01_basics/06_numAndMath.js
@@ -0,0 +1,12 @@
+const score  = 400
+
+const balance = new Number(100)
+console.log(balance);
+console.log(balance.toString());
+console.log(typeof(balance.toString().length));
+console.log(typeof(balance.toFixed(2)));
+
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-US'));
+
